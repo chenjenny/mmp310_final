c2916b629cd825cfdf1fd8e2e08eae27ab04884f
= <sarahsooklall@yahoo.com>
2018-05-02 15:50:48 -0400

    Beginning of game

---
+++ app.js
@@ -0,0 +1,38 @@
+/*
+** Document Ready Event (Type All Code in There)
+*/
+$(document).ready(function() {
+    /*
+    ** Api 
+    */
+    var key = "G27cFqltoAFa6grAhM6gJv94Nbk5zdiL"; /* Key from personal Giphy account */
+    var urlOne = "https://api.giphy.com/v1/gifs/search?q="
+    /*
+    ** Start of Game - Text Slideshow 
+    */
+//    var beginningText = new Array();
+//    var time = 0;
+//    beginningText[0] = "Welcome";
+//    beginningText[1] = "To";
+//    beginningText[2] = "Name";
+//    beginningText[3] = "Game";
+//    beginningText[4] = "Let's Begin!";
+//    loop();
+//    function loop() {
+//        $('#slideshow').html(beginningText[time]);
+//        time++;
+//        // Change image every 0.8 seconds
+//        setTimeout(loop, 800);
+//    }
+//    /*
+//    ** Start Button Display 
+//    */
+//    function displayStartbtn() {
+//        if (beginningText[time] == 5){
+//            document.getElementById('slide').style.display = "block";
+//        }
+//    }
+    
+    
+    
+});
