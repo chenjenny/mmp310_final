ae71e777a097f8fa174d6a4c91c2ccd7bc4fc337
Sarah Sooklall <sarah.sooklall@f1006-04-34124.bmcc.cuny.edu>
2018-05-02 19:18:35 -0400

    brainstorm of ideas

--- app.js
+++ app.js
@@ -2,37 +2,38 @@
 ** Document Ready Event (Type All Code in There)
 */
 $(document).ready(function() {
-    /*
-    ** Api 
-    */
-    var key = "G27cFqltoAFa6grAhM6gJv94Nbk5zdiL"; /* Key from personal Giphy account */
-    var urlOne = "https://api.giphy.com/v1/gifs/search?q="
-    /*
-    ** Start of Game - Text Slideshow 
-    */
-//    var beginningText = new Array();
-//    var time = 0;
-//    beginningText[0] = "Welcome";
-//    beginningText[1] = "To";
-//    beginningText[2] = "Name";
-//    beginningText[3] = "Game";
-//    beginningText[4] = "Let's Begin!";
-//    loop();
-//    function loop() {
-//        $('#slideshow').html(beginningText[time]);
-//        time++;
-//        // Change image every 0.8 seconds
-//        setTimeout(loop, 800);
-//    }
-//    /*
-//    ** Start Button Display 
-//    */
-//    function displayStartbtn() {
-//        if (beginningText[time] == 5){
-//            document.getElementById('slide').style.display = "block";
-//        }
+    
+//    var canvas = document.getElementById("canvas");
+//    var ctx = canvas.getContext('2d');
+//
+//    
+//    if (ctx) {
+//	   ctx.fillStyle = "blue";
+//	   ctx.fillRect(100, 100, 50, 50);
+//	   ctx.fillStyle = "green";
+//	   ctx.fillRect(200, 100, 50, 50);
+//	   ctx.font = "40px Comic Sans MS"
+//	   ctx.fillText("My Game!", 10, 200);
 //    }
     
+//    $(".submit").click(function(){
+//		        	var city = $(".city").val()
+//		        	//Make the API Call
+//		        	$.getJSON("http://api.openweathermap.org/data/2.5/weather",
+//				{
+//					zip:11355,
+//					units:"imperial"
+//				},
+//				function(response){  //'response' is actually a variable, we could name it what ever we want. It just stores what the api sends
+//					console.log(response)
+//					$(".country").html(response.sys.country)
+//					$(".temp").html(response.main.temp);
+//					$(".city-name").html(response.name);
+//					  $(".weather").html(response.weather[0].description)
+//				});
+//	        })
     
+    
+
     
 });
