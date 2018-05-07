c3a2fbb44a64e05f1a8ae3f48b71300fbe4dd052
= <sarahsooklall@yahoo.com>
2018-05-07 16:05:57 -0400

    working New York weather, deleted p5 files (not needed for this project).

--- app.js
+++ app.js
@@ -1,39 +1,21 @@
 /*
 ** Document Ready Event (Type All Code in There)
 */
-$(document).ready(function() {
-    
-//    var canvas = document.getElementById("canvas");
-//    var ctx = canvas.getContext('2d');
-//
-//    
-//    if (ctx) {
-//	   ctx.fillStyle = "blue";
-//	   ctx.fillRect(100, 100, 50, 50);
-//	   ctx.fillStyle = "green";
-//	   ctx.fillRect(200, 100, 50, 50);
-//	   ctx.font = "40px Comic Sans MS"
-//	   ctx.fillText("My Game!", 10, 200);
-//    }
-    
-//    $(".submit").click(function(){
-//		        	var city = $(".city").val()
-//		        	//Make the API Call
-//		        	$.getJSON("http://api.openweathermap.org/data/2.5/weather",
-//				{
-//					zip:11355,
-//					units:"imperial"
-//				},
-//				function(response){  //'response' is actually a variable, we could name it what ever we want. It just stores what the api sends
-//					console.log(response)
-//					$(".country").html(response.sys.country)
-//					$(".temp").html(response.main.temp);
-//					$(".city-name").html(response.name);
-//					  $(".weather").html(response.weather[0].description)
-//				});
-//	        })
-    
-    
-
-    
+$(document).ready(function() {   
+    $(".submit").click(function(){
+		        	var city = $(".city").val()
+		        	//Make the API Call
+		        	$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=0db405f4f4c3f54c4a45e42c5a0788a2",
+//                    $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=40.7128&lon=74.0060&cnt=50&APPID=0db405f4f4c3f54c4a45e42c5a0788a2",
+				{
+					units:"imperial"
+				},
+				function(response){
+					console.log(response)
+					$(".country").html(response.sys.country)
+					$(".temp").html(response.main.temp);
+					$(".city-name").html(response.name);
+				    $(".weather").html(response.weather[0].description)
+				});
+	        })
 });
