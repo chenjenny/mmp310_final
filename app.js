a8cab66d108486d97a06afc9b926ab0cd3fa188b
Sarah Sooklall <sarah.sooklall@f1006-04-34124.bmcc.cuny.edu>
2018-05-07 19:25:23 -0400

    upadte

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ $(document).ready(function() {
     $(".submit").click(function(){
 		        	var city = $(".city").val()
 		        	//Make the API Call
-		        	$.getJSON("http://api.openweathermap.org/data/2.5/weather?id=5128638&APPID=0db405f4f4c3f54c4a45e42c5a0788a2",
-//                    $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=40.7128&lon=74.0060&cnt=50&APPID=0db405f4f4c3f54c4a45e42c5a0788a2",
+		        	$.getJSON("http://api.openweathermap.org/data/2.5/weather?",
+//                    $.getJSON("http://api.openweathermap.org/data/2.5/find?lat=40.7128&lon=74.0060&cnt=50",
 				{
-					units:"imperial"
+                    APPID:"0db405f4f4c3f54c4a45e42c5a0788a2",
+//					units:"imperial",
+                    id:"5128638"
+//                    lat:"40.7128",
+//                    lon:"74.0060",
+//                    cnt:"50"
+                    
 				},
 				function(response){
-					console.log(response)
-					$(".country").html(response.sys.country)
+					console.log(response);
+					$(".country").html(response.sys.country);
 					$(".temp").html(response.main.temp);
 					$(".city-name").html(response.name);
-				    $(".weather").html(response.weather[0].description)
+				    $(".weather").html(response.weather[0].description);
 				});
 	        })
 });
