eea458bd9a5e49633e279e55fff59ca3c5528113
= <sarahsooklall@yahoo.com>
2018-05-14 16:37:07 -0400

    added map

--- app.js
+++ app.js
@@ -104,10 +104,10 @@ $(document).ready(function() {
             // For country/city
             $.ajax(params).done(function(response){
                 // Show card
-                $('.card').show();
+                $('.weather').show();
                 
                 // Title
-                $('.card-title').text(response.name);
+                $('.weather-title').text(response.name);
                 
                 // Images 
                 var image = response.weather[0].icon;
@@ -142,59 +142,151 @@ $(document).ready(function() {
         });
     });
 });
-function initMap() {
-    // Set Latitude and Longitude
-	var algeria = {
-		lat: 28.0339,
-		lng: 1.6596
-	};
-    var newYork = {
-		lat: 40.730610,
-		lng: -73.935242
-	};
-    var russia = {
-		lat: 62.40091936118402,
-		lng: 94.64788124999995
-	};
-    // Connect to HTML to show map
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 2,
-		center: algeria
-	});
-	// Add marker
-	var markerOne = new google.maps.Marker({
-		position: algeria,
-		map: map,
-		title: "Algeria"
-	});
-    var markerTwo = new google.maps.Marker({
-		position: newYork,
-		map: map,
-		title: "New York"
-	});
-    var markerThree = new google.maps.Marker({
-		position: russia,
-		map: map,
-		title: "Russia"
-	});
-	// Clicked zoom to position centered
-	markerOne.addListener("click", function() {
-		map.setZoom(7);
-		map.setCenter(markerOne.getPosition());
-	});
-    markerTwo.addListener("click", function() {
-		map.setZoom(7);
-		map.setCenter(markerTwo.getPosition());
-	});
-    markerThree.addListener("click", function() {
-		map.setZoom(7);
-		map.setCenter(markerThree.getPosition());
-	});
-	// show lat & lng where clicked on map in console
-	map.addListener("click", function(event) {
-		console.log(event);
-		console.log(event.latLng.lat());
-		console.log(event.latLng.lng());
-	});
-	
-}
+
+var map;
+  var geoJSON;
+  var request;
+  var gettingData = false;
+  var openWeatherMapKey = "27659eba6e7ec121ab63b30de42a931a"
+  function initialize() {
+    var mapOptions = {
+      zoom: 2,
+      center: new google.maps.LatLng(40.730610,-73.935242)
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+//      var newYork = {
+//		lat: 40.730610,
+//		lng: -73.935242
+//	};
+//      var marker = new google.maps.Marker({
+//		position: newYork,
+//		map: map,
+//		title: "New York"
+//	});
+//      marker.addListener("click", function() {
+//		map.setZoom(7);
+//		map.setCenter(marker.getPosition());
+//	});
+//      var map = new google.maps.Map(document.getElementById('map-canvas'), {
+//		zoom: 2,
+//		center: newYork
+//	});
+    // Add interaction listeners to make weather requests
+    google.maps.event.addListener(map, 'idle', checkIfDataRequested);
+    // Sets up and populates the info window with details
+    map.data.addListener('click', function(event) {
+      infowindow.setContent(
+       "<img src=" + event.feature.getProperty("icon") + ">"
+       + "<br /><strong>" + event.feature.getProperty("city") + "</strong>"
+       + "<br />" + event.feature.getProperty("temperature") + "&deg;C"
+       + "<br />" + event.feature.getProperty("weather")
+       );
+      infowindow.setOptions({
+          position:{
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+          },
+          pixelOffset: {
+            width: 0,
+            height: -15
+          }
+        });
+      infowindow.open(map);
+    });
+  }
+  var checkIfDataRequested = function() {
+    // Stop extra requests being sent
+    while (gettingData === true) {
+      request.abort();
+      gettingData = false;
+    }
+    getCoords();
+  };
+  // Get the coordinates from the Map bounds
+  var getCoords = function() {
+    var bounds = map.getBounds();
+    var NE = bounds.getNorthEast();
+    var SW = bounds.getSouthWest();
+    getWeather(NE.lat(), NE.lng(), SW.lat(), SW.lng());
+  };
+  // Make the weather request
+  var getWeather = function(northLat, eastLng, southLat, westLng) {
+    gettingData = true;
+    var requestString = "http://api.openweathermap.org/data/2.5/box/city?bbox="
+                        + westLng + "," + northLat + "," //left top
+                        + eastLng + "," + southLat + "," //right bottom
+                        + map.getZoom()
+                        + "&cluster=yes&format=json"
+                        + "&APPID=" + openWeatherMapKey;
+    request = new XMLHttpRequest();
+    request.onload = proccessResults;
+    request.open("get", requestString, true);
+    request.send();
+  };
+  // Take the JSON results and proccess them
+  var proccessResults = function() {
+    console.log(this);
+    var results = JSON.parse(this.responseText);
+    if (results.list.length > 0) {
+        resetData();
+        for (var i = 0; i < results.list.length; i++) {
+          geoJSON.features.push(jsonToGeoJson(results.list[i]));
+        }
+        drawIcons(geoJSON);
+    }
+  };
+  var infowindow = new google.maps.InfoWindow();
+  // For each result that comes back, convert the data to geoJSON
+  var jsonToGeoJson = function (weatherItem) {
+    var feature = {
+      type: "Feature",
+      properties: {
+        city: weatherItem.name,
+        weather: weatherItem.weather[0].main,
+        temperature: weatherItem.main.temp,
+        min: weatherItem.main.temp_min,
+        max: weatherItem.main.temp_max,
+        humidity: weatherItem.main.humidity,
+        pressure: weatherItem.main.pressure,
+        windSpeed: weatherItem.wind.speed,
+        windDegrees: weatherItem.wind.deg,
+        windGust: weatherItem.wind.gust,
+        icon: "http://openweathermap.org/img/w/"
+              + weatherItem.weather[0].icon  + ".png",
+        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+      },
+      geometry: {
+        type: "Point",
+        coordinates: [weatherItem.coord.Lon, weatherItem.coord.Lat]
+      }
+    };
+    // Set the custom marker icon
+    map.data.setStyle(function(feature) {
+      return {
+        icon: {
+          url: feature.getProperty('icon'),
+          anchor: new google.maps.Point(25, 25)
+        }
+      };
+    });
+    // returns object
+    return feature;
+  };
+  // Add the markers to the map
+  var drawIcons = function (weather) {
+     map.data.addGeoJson(geoJSON);
+     // Set the flag to finished
+     gettingData = false;
+  };
+  // Clear data layer and geoJSON
+  var resetData = function () {
+    geoJSON = {
+      type: "FeatureCollection",
+      features: []
+    };
+    map.data.forEach(function(feature) {
+      map.data.remove(feature);
+    });
+  };
+  google.maps.event.addDomListener(window, 'load', initialize);
